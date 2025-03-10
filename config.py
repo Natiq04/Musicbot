bf1eefbbce15637aa9583b309cdd6e2a2e51d54f
Natiq04 <167504381+Natiq04@users.noreply.github.com>
2025-03-10 19:41:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = 20235406
 API_HASH = "968860b04772cb5b44f4ca8603bed9d3"
 BOT_TOKEN = "6388818920:AAE5GYIPOzBoGeLn76HxfaiH1WHDV9jfhlQ"
-MONGO_DB_URI = "mongodb+srv://SumgaitProject:SumgaitProject@sumgaitproject.daihrs3.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://xguliyev:xguliyev@iconmusicbot.5icrs.mongodb.net/?retryWrites=true&w=majority&appName=iconmusicbot"
 DURATION_LIMIT_MIN = 45
 LOGGER_ID = -1002150762661
 OWNER_ID = 7287936548
